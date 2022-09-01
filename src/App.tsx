3c33ce3ee7f1aacfc1f9cdfc83a6bdc85e3c330c
Ector Cunha <ectorvccunha@outlook.com>
2022-09-01 17:11:56 -0300

    usando react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { ReactElement } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Pokemon } from "./Pokemon";
 import { IPokemon } from "./types";
-import "./App.css";
 import { PokemonFetch } from "./PokemonFetch";
+import "./App.css";
 
-
+const queryClient = new QueryClient();
 
 export function App(props: IPokemon): ReactElement {
   return (
+    <QueryClientProvider client={queryClient}>
    <PokemonFetch/>
+   </QueryClientProvider>
   );
 }
 
--- src/PokemonFetch.tsx
+++ src/PokemonFetch.tsx
@@ -1,52 +1,58 @@
 import { ReactElement, useEffect, useState } from "react";
-
+import { useQuery } from "react-query";
+import { IPokemon } from "./types";
 
 interface Pokemon {
-    id?: number;
-    name: string;
-    url: string;
-    image: string;
+  id?: number;
+  name: string;
+  url: string;
+  image: string;
 }
 
 async function getPokemons(): Promise<Pokemon[]> {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=30&offset=1');
+  try {
+    const response = await fetch(
+      "https://pokeapi.co/api/v2/pokemon?limit=30&offset=1"
+    );
     const data = await response.json();
     return data.results;
-}
 
-async function getPokemonsImage(): Promise<Pokemon[]> {
-    const resp = await fetch('https://pokeapi.co/api/v2/pokemon?/url/sprities/dream_world/front_default');
-    const data = await resp.json();
-    return data.results;
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
 }
 
 export function PokemonFetch(): ReactElement {
-    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
-    const [pokemonsImage, setPokemonsImage] = useState<Pokemon[]>([]);
-    
-    useEffect(() => {
-        getPokemons().then((pokemons) => setPokemons(pokemons));
-        getPokemonsImage().then((pokemons) => setPokemonsImage(pokemons));
-    }, []);
-    return (
-        <div className="cards">
-            <h1>Pokemons direto da API</h1>
-            {pokemons.map((pokemon) => {
-                return (
-                    <div className="card" key={pokemon.id}>
-                        {
-                            pokemonsImage.map((pokemon) => {
-                                return (
-                                    <img src={pokemon.image} alt={pokemon.name} />
-                                )
-                            })
-                        }
-                        <img src={pokemon.image} alt="Imagem do pokemon" />
-                        <h2>{pokemon.name}</h2>
-                        <p>{pokemon.url}</p>
-                    </div>
-                );
-            })}
-        </div>
-    );
+    const { data, isLoading, error } = useQuery<Pokemon[], Error>(["pokemons"], ()=> getPokemons());
+
+    if(isLoading) return <div>Loading...</div>
+    if(error) return <div>Não podemos carregar os personagens agora. Tente novamente mais tarde.</div>
+
+//   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+//   const [pokemonsImage, setPokemonsImage] = useState<Pokemon[]>([]);
+
+//   useEffect(() => {
+//     getPokemons().then((pokemons) => setPokemons(pokemons));
+//     // getPokemonsImage().then((pokemons) => setPokemonsImage(pokemons));
+//   }, []);
+
+
+  return (
+    <div className="cards">
+      <h1>Pokemons direto da API</h1>
+      {data?.map((pokemon: Pokemon) => {
+        return (
+          <div className="card" key={pokemon.id}>
+            {/* {data.results.map((pokemon) => {
+              return <img src={pokemon.image} alt={pokemon.name} />;
+            })} */}
+            <img src={pokemon.image} alt="Imagem do pokemon" />
+            <h2>{pokemon.name}</h2>
+            <p>{pokemon.url}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
