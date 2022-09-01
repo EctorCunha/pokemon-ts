819d03ead2e591d5f46091607978376d47f3b29e
Ector Cunha <ectorvccunha@outlook.com>
2022-08-31 22:50:21 -0300

    tentando add imagem

--- src/PokemonFetch.tsx
+++ src/PokemonFetch.tsx
@@ -5,19 +5,28 @@ interface Pokemon {
     id?: number;
     name: string;
     url: string;
+    image: string;
 }
 
 async function getPokemons(): Promise<Pokemon[]> {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=200');
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=30&offset=1');
     const data = await response.json();
+    return data.results;
+}
+
+async function getPokemonsImage(): Promise<Pokemon[]> {
+    const resp = await fetch('https://pokeapi.co/api/v2/pokemon?/url/sprities/dream_world/front_default');
+    const data = await resp.json();
     return data.results;
 }
 
 export function PokemonFetch(): ReactElement {
     const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+    const [pokemonsImage, setPokemonsImage] = useState<Pokemon[]>([]);
     
     useEffect(() => {
-        getPokemons().then((characters) => setPokemons(characters));
+        getPokemons().then((pokemons) => setPokemons(pokemons));
+        getPokemonsImage().then((pokemons) => setPokemonsImage(pokemons));
     }, []);
     return (
         <div className="cards">
@@ -25,6 +34,14 @@ export function PokemonFetch(): ReactElement {
             {pokemons.map((pokemon) => {
                 return (
                     <div className="card" key={pokemon.id}>
+                        {
+                            pokemonsImage.map((pokemon) => {
+                                return (
+                                    <img src={pokemon.image} alt={pokemon.name} />
+                                )
+                            })
+                        }
+                        <img src={pokemon.image} alt="Imagem do pokemon" />
                         <h2>{pokemon.name}</h2>
                         <p>{pokemon.url}</p>
                     </div>
